1bc96591a6600c8ddd21c494a8f47adfe39f5421
Jamie Pham <jamie.pham@outlook.com>
2026-08-31 18:01:14 +0800

    Test Case 1
    Get Empty Wallet Balance

--- Controllers/WalletsController.cs
+++ Controllers/WalletsController.cs
@@ -7,6 +7,32 @@ namespace WalletAPI.Controllers;
 [Route("[controller]")]
 public class WalletsController : ControllerBase
 {
+    private static readonly List<Wallet> wallets =
+    [
+        new() {
+            transactionId = "tx101",
+            coins = 100,
+        },
+        new() {
+            transactionId = "tx103",
+            coins = 0,
+        }
+    ];
+    [HttpGet]
+    public ActionResult<List<Wallet>> GetWallets()
+    {
+        return Ok(wallets);
+    }
+    [HttpGet("{id}")]
+    public ActionResult<List<Wallet>> GetWalletById(string id)
+    {
+        var wallet = wallets.FirstOrDefault(x => x.transactionId == id);
+        if (wallet is null)
+        {
+            return NotFound();
+        }
 
+        return Ok(wallet);
+    }
 }
 
